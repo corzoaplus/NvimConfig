d5a6006a05ad9cd52440b00fc9e9fa4b34e0d746
corzoaplus <corzoaplus@gmail.com>
2025-04-06 18:02:10 -0500

    Configuracion nvim

---
+++ lua/configs/lazy.lua
@@ -0,0 +1,277 @@
+return {
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.6",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		opts = function()
+			return require("configs.telescope")
+		end,
+		--function(_,opts)
+		--	    require("telescope").setup(opts)
+		--  end
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			local configs = require("nvim-treesitter.configs")
+
+			-- Base languages for all platforms
+			local base_languages = {
+				"vim",
+				"lua",
+				"vimdoc",
+				"html",
+				"css",
+				"javascript",
+				"typescript",
+				"tsx",
+				"rust",
+				"svelte",
+				"python",
+				"yaml",
+			}
+
+			-- Unix-only languages
+			local unix_languages = { "c", "cpp", "zig" }
+
+			-- Combine languages based on platform
+			local ensure_installed = base_languages
+			if vim.fn.has("unix") == 1 then
+				vim.list_extend(ensure_installed, unix_languages)
+			end
+
+			configs.setup({
+				ensure_installed = ensure_installed,
+				sync_install = false,
+				highlight = { enable = true },
+				indent = { enable = true },
+			})
+		end,
+	},
+	{
+		"EdenEast/nightfox.nvim",
+		lazy = false, -- make sure we load this during startup if it is your main colorscheme
+		priority = 1000, -- make sure to load this before all the other start plugins
+		config = function()
+			-- load the colorscheme here
+			--vim.cmd.colorscheme("duskfox")
+		end,
+	},
+	{
+		"williamboman/mason.nvim",
+		cmd = { "Mason", "MasonInstall", "MasonInstallAll", "MasonUpdate", "MasonUninstallAll" },
+		opts = function()
+			return require("configs.mason")
+		end,
+		config = function(_, opts)
+			require("mason").setup(opts)
+			vim.api.nvim_create_user_command("MasonInstallAll", function()
+				if opts.ensure_installed and #opts.ensure_installed > 0 then
+					vim.cmd("MasonInstall " .. table.concat(opts.ensure_installed, " "))
+				end
+			end, {})
+			vim.g.mason_binaries_list = opts.ensure_installed
+		end,
+	},
+	{ "williamboman/mason-lspconfig.nvim" },
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			{ "williamboman/mason.nvim" },
+			{ "williamboman/mason-lspconfig.nvim" },
+		},
+		config = function()
+			require("configs.lspconfig")
+		end,
+	},
+	{
+		"nvim-tree/nvim-tree.lua",
+		version = "*",
+		lazy = false,
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+		},
+		opts = function()
+			return require("configs.nvimtree")
+		end,
+		config = function(_, opts)
+			require("nvim-tree").setup(opts)
+		end,
+	},
+	{
+		-- Thank you to Josean for his video https://youtu.be/NL8D8EkphUw?si=3ZAt7ZJ0S1HuDJ_M
+		"hrsh7th/nvim-cmp",
+		event = "InsertEnter",
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			"L3MON4D3/LuaSnip",
+			"saadparwaiz1/cmp_luasnip",
+			"rafamadriz/friendly-snippets",
+		},
+		opts = function()
+			return require("configs.nvim-cmp")
+		end,
+		config = function()
+			local cmp = require("cmp")
+			local luasnip = require("luasnip")
+			require("luasnip.loaders.from_vscode").lazy_load()
+
+			cmp.setup({
+				completion = {
+					completeopt = "menu,menuone,preview,noselect",
+				},
+				snippet = { -- configure how nvim-mp interacts with snippet engine
+					expand = function(args)
+						luasnip.lsp_expand(args.body)
+					end,
+				},
+				mapping = cmp.mapping.preset.insert({
+					["<C-K>"] = cmp.mapping.select_prev_item(), --previous suggestion
+					["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
+					["<C-b>"] = cmp.mapping.scroll_docs(-4),
+					["<C-f>"] = cmp.mapping.scroll_docs(4),
+					["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
+					["<C-e>"] = cmp.mapping.abort(), -- close completion window
+					["<CR>"] = cmp.mapping.confirm({ select = false }),
+				}),
+				sources = cmp.config.sources({
+					{ name = "nvim_lsp" },
+					{ name = "luasnip" },
+					{ name = "buffer" },
+					{ name = "path" },
+				}),
+			})
+		end,
+	},
+	{
+		"stevearc/conform.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		opts = function()
+			return require("configs.formating")
+		end,
+		config = function(_, opts)
+			require("conform").setup(opts)
+		end,
+	},
+	{
+		"rust-lang/rust.vim",
+		ft = "rust",
+		init = function()
+			vim.g.rustfmt_autosave = 1
+		end,
+	},
+	{
+		"mrcjkb/rustaceanvim",
+		version = "^4", -- Recommended
+		lazy = false, -- This plugin is already lazy
+		ft = "rust",
+		dependencies = {
+			"neovim/nvim-lspconfig",
+		},
+		config = function()
+			local bufnr = vim.api.nvim_get_current_buf()
+			vim.keymap.set("n", "<leader>a", function()
+				vim.cmd.RustLsp("codeAction") -- supports rust-analyzer's grouping
+				vim.cmd.RustLsp("debug")
+				vim.cmd.RustLsp("debuggables")
+				-- or, to run the previous debuggable:
+				vim.cmd.RustLsp({ "debuggables", bang = true })
+				-- or, to override the executable's args:
+				vim.cmd.RustLsp({ "debuggables", "arg1", "arg2" })
+				-- or vim.lsp.buf.codeAction() if you don't want grouping.
+			end, { silent = true, buffer = bufnr })
+			vim.api.nvim_create_user_command("RustDebuggables", function()
+				vim.cmd("RustLsp debuggables")
+			end, {})
+		end,
+	},
+	{
+		"mfussenegger/nvim-dap",
+		config = function()
+			local dap = require("dap")
+			vim.api.nvim_create_user_command("DapSidebar", function()
+				local widgets = require("dap.ui.widgets")
+				local sidebar = widgets.sidebar(widgets.scopes)
+				sidebar.open()
+			end, {})
+		end,
+	},
+	{
+		"mfussenegger/nvim-dap-python",
+		ft = "python",
+		dependencies = {
+			"mfussenegger/nvim-dap",
+			"nvim-neotest/nvim-nio",
+		},
+		config = function()
+			local path = "~/.local/share/nvim/mason/packages/debugpy/venv/bin/python"
+			require("dap-python").setup(path)
+		end,
+	},
+	{
+		"rcarriga/nvim-dap-ui",
+		dependencies = { "mfussenegger/nvim-dap" },
+		config = function()
+			local dap, dapui = require("dap"), require("dapui")
+
+			dapui.setup()
+
+			dap.listeners.before.attach.dapui_config = function()
+				dapui.open()
+			end
+			dap.listeners.before.launch.dapui_config = function()
+				dapui.open()
+			end
+			dap.listeners.before.event_terminated.dapui_config = function()
+				dapui.close()
+			end
+			dap.listeners.before.event_exited.dapui_config = function()
+				dapui.close()
+			end
+		end,
+	},
+
+	{
+		"mxsdev/nvim-dap-vscode-js",
+		dependencies = {
+			"mfussenegger/nvim-dap",
+		},
+		opts = function()
+			return require("configs.dap_js")
+		end,
+		config = function(_, opts)
+			require("dap-vscode-js").setup(opts)
+		end,
+	},
+	{
+		"oxfist/night-owl.nvim",
+		lazy = true, --false, -- make sure we load this during startup if it is your main colorscheme
+		--priority = 1000, -- make sure to load this before all the other start plugins
+		config = function()
+			-- load the colorscheme here
+			require("night-owl").setup() -- You can pass in your personal settings here.
+			-- vim.cmd.colorscheme("night-owl")
+		end,
+	},
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			vim.cmd.colorscheme("tokyonight-storm")
+		end,
+	},
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		opts = function()
+			return require("configs.lualine")
+		end,
+		config = function(_, opts)
+			require("lualine").setup(opts)
+		end,
+	},
+}
